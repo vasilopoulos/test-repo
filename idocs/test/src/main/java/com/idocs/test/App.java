23f1b26a567c2ab6dd0eae05baa3b2cc7d7c4d3c
vasilopoulos <george.vassilopoulos@gmail.com>
2013-10-27 21:30:28 +0000

    initial

---
+++ idocs/test/src/main/java/com/idocs/test/App.java
@@ -0,0 +1,13 @@
+package com.idocs.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
